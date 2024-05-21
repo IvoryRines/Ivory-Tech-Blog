5bfabe9ac77e34aaa20eb3d0109ceb15ce880964
EyeVorE <subplot_05tracer@icloud.com>
2024-05-20 23:29:27 -0500

    Added some routes and filled in connection.js, schema.sql, and auth.js.

---
+++ controllers/index.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes");
+
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+
+module.exports = router;
+
+// const router = require("express").Router();
+// const homeRoutes = require("./homeRoutes");
+
+// router.use("/", homeRoutes);
+
+// module.exports = router;
